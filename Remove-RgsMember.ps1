bb6a8c87d7e3e35b37b0d7f3e9b05d52ceb1a706
Matt Hickok <matt@mhickok.me>
2017-03-23 16:37:59 -0500

    added some error handling,parameter sets

--- Remove-RgsMember.ps1
+++ Remove-RgsMember.ps1
@@ -5,13 +5,15 @@
 
         [Parameter(Mandatory=$True,
                     ValueFromPipeline=$True,
-                    HelpMessage="User to remove (sip:user.sipdomain")]
+                    HelpMessage="User(s) to remove (sip:user.sipdomain")]
         [string]$User,
 
+        [Parameter(ParameterSetName="specifiedGroups",
+                    HelpMessage="One or more RGs, separated by commas")]
         [string[]]$ResponseGroup,
 
-        [string]$LogLocation,
-
+        [Parameter(ParameterSetName="allGroups",
+                    HelpMessage="Remove the user(s) from all RGs")]
         [switch]$AllGroups
     )
 
@@ -19,27 +21,57 @@
 
     PROCESS {
 
+        #Removing user(s) from all RGs
         if($AllGroups) {
-
+            Write-Verbose -Message "User(s) will be removed from all response groups"
             foreach ($member in $User) {
-                $groupsContainingUser = Get-CsRgsAgentGroup | Where-Object {$_.AgentsByUri -contains $member}
-                foreach ($group in $groupsContainingUser) {    
-                    $group.AgentsByUri.Remove($member)  
-                    Set-CsRgsAgentGroup -Instance $group  
-                }      
+                Write-Verbose -Message "Removing $member"
+                try {
+                    $groupsContainingUser = Get-CsRgsAgentGroup | Where-Object {$_.AgentsByUri -contains $member}
+                    Write-Verbose -Message "$member is in $($groupsContainingUser.Count) groups"
+                }
+                catch {
+                    Write-Verbose "Unable to get RG information for user "
+                }
+
+                if($groupsContainingUser.Count -gt 0) {
+                    foreach ($group in $groupsContainingUser) {
+                        try{
+                            Write-Verbose -Message "Removing $member from $($group.Name)"
+                            $group.AgentsByUri.Remove($member)  
+                            Set-CsRgsAgentGroup -Instance $group
+                        }
+                        catch {
+                            Write-Verbose -Message "Unable to remove $member from $($group.Name)"
+                        }
+                    }
+                }
+
+                else {
+                    Write-Verbose "Not attempting removal as the $member was not in any RGs"
+                }
+   
             }
         }
 
+        #Remove user(s) only from specific RGs
         else {
+            Write-Verbose -Message "User(s) will be removed from specified RGs"
             foreach ($member in $User) {
+                Write-Verbose -Message "Starting removals for $member"
                 foreach($group in $ResponseGroup) {
-                    $rg = Get-CsRgsAgentGroup -Name $group
-                    $rg.AgentsByUri.Remove($member)
-                    Set-CsRgsAgentGroup -Instance $rg 
+                    try{
+                        Write-Verbose -Message "Removing $member from $group"
+                        $rg = Get-CsRgsAgentGroup -Name $group
+                        $rg.AgentsByUri.Remove($member)
+                        Set-CsRgsAgentGroup -Instance $rg 
+                    }
+                    catch {
+                        Write-Verbose -Message "Unable to remove $member from $group"
+                    }
                 }
             }
         }
-
     }
 
     END {}
